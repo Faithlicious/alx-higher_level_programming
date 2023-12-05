2421a1d4069e984091e46b5703fbeb1bc8d74041
faithlicious <violetshiko15@gmail.com>
2023-12-04 17:53:08 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python3
-no_c = __import__('5-no_c').no_c
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
 
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+    lenn = len(my_string)
+
+    for i in range(lenn):
+        if ((my_string[i] != 'c') and (my_string[i] != 'C')):
+            new_str += my_string[i]
+    return new_str
