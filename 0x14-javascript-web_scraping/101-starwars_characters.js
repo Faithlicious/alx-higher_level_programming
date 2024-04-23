f86801a0ac26683002fec5af4cac874561ec8d50
Faithlicious <violetshiko15@gmail.com>
2024-04-23 04:34:07 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,51 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-  const charactersUrls = filmData.characters;
-
-  // Function to fetch character names and print them
-  function fetchAndPrintCharacter(index) {
-    if (index >= charactersUrls.length) {
-      return;
-    }
-
-    const characterUrl = charactersUrls[index];
-    request(characterUrl, function (error, response, body) {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error('Request failed with status code:', response.statusCode);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-
-      // Fetch next character recursively
-      fetchAndPrintCharacter(index + 1);
-    });
-  }
-
-  // Start fetching characters
-  fetchAndPrintCharacter(0);
 });
 
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
